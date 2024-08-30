6737fd9e392a23b5c18fb35336aa2995ba28becf
Reima Riihimäki <reimarii@oamk.fi>
2024-08-30 22:26:03 +0300

    init

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>REACT</h1>
+      <ul>
+        <li>Components</li>
+        <li>Events</li>
+        <li>State management</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ react-component-exercise/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ScoreList/>
+    </div>
+  );
+}
+
+/**
+ * This component is the Header UI
+ */
+function Header() {
+  return (
+    <div>
+      <h1>PLAYER SCORES</h1>
+      <p>Here are the final points of the game</p>
+    </div>
+  );
+}
+
+/**
+ * This component is the score list UI
+ */
+function ScoreList() {
+  return (
+    <div>
+      <hr />
+      <h3>Name:</h3>
+      <p>Reima</p>
+      <h3>Points:</h3>
+      <p>35</p>
+      <hr />
+      <h3>Name:</h3>
+      <p>Liisa</p>
+      <h3>Points:</h3>
+      <p>42</p>
+      <hr />
+    </div>
+  );
+}
+
+
+export default App
